33af5739850d016ab703928d03e1258d54e816f8
lesses <767733103@qq.com>
2017-09-16 22:35:14 +0800

    开发环境50%

---
+++ script/a-util.js
@@ -0,0 +1,7 @@
+ define(["./util.js"], function(util) {
+ 	return {
+ 		aGetData: function(date) {
+ 			return util.getData(date, 2);
+ 		}
+ 	}
+ });
---
+++ script/util.js
@@ -0,0 +1,12 @@
+define(function() {
+    return {
+        getData: function(date, type) {
+            if (type === 1) {
+                return "2017-09-16";
+            };
+            if (type === 2) {
+                return "2017年9月16日";
+            }
+        }
+    }
+});
